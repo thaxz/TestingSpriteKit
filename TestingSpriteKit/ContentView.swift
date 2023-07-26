bba05d4139ceffa496876e6826f48301a0065aa7
thaxz <thaxzt@gmail.com>
2023-07-26 17:45:13 -0300

    first commit

---
+++ TestingSpriteKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TestingSpriteKit
+//
+//  Created by thaxz on 26/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TestingSpriteKit/TestingSpriteKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestingSpriteKitApp.swift
+//  TestingSpriteKit
+//
+//  Created by thaxz on 26/07/23.
+//
+
+import SwiftUI
+
+@main
+struct TestingSpriteKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
